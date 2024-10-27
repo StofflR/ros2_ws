19cbb90699a9e66949bba222339936fe88bde244
StofflR <stephan.robinig@edu.uni-graz.at>
2024-10-27 19:06:35 +0100

    ex3

--- src/husky_highlevel_controller/launch/launch.py
+++ src/husky_highlevel_controller/launch/launch.py
@@ -1,7 +1,7 @@
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
@@ -16,22 +16,23 @@ def generate_launch_description():
     sim_launch = PathJoinSubstitution(
         [pkg_clearpath_gz, "launch", "simulation.launch.py"]
     )
-
+    
     # Include simulation launch file and start rviz
     sim = IncludeLaunchDescription(PythonLaunchDescriptionSource([sim_launch]),
             launch_arguments=[
-            ('rviz', 'true')]
+            ('rviz', 'true'),
+            ('world', 'single_pillar')]
     )
 
     # add teleopt_twist_keyboard with launch prefix gnome-terminal --
-    teleopt_node = Node(
-        package="teleop_twist_keyboard",
-        executable="teleop_twist_keyboard",
-        name="teleop",
-        output="screen",
-        prefix="gnome-terminal --",
-        namespace="a200_0000",
-    )
+    #teleopt_node = Node(
+    #    package="teleop_twist_keyboard",
+    #    executable="teleop_twist_keyboard",
+    #    name="teleop",
+    #    output="screen",
+    #    prefix="gnome-terminal --",
+    #    namespace="a200_0000",
+    #)
     # load params from yaml file
     param_file = os.path.join(
         os.getcwd(),
@@ -47,13 +48,16 @@ def generate_launch_description():
         name="husky_highlevel",
         output="screen",
         prefix="gnome-terminal --",
+        parameters=[param_file],
         namespace="a200_0000",
-        parameters=[param_file]
+        # remap tf and tf static to a200_0000
+        remappings=[('/tf', '/a200_0000/tf'), ('/tf_static', '/a200_0000/tf_static')]
+        
     )
 
     # Create launch description and add actions
     ld = LaunchDescription()
     ld.add_action(sim)
-    ld.add_action(teleopt_node)
+    # ld.add_action(teleopt_node)
     ld.add_action(high_level)
     return ld
--- src/husky_highlevel_controller/src/subscriber.cpp
+++ src/husky_highlevel_controller/src/subscriber.cpp
@@ -3,6 +3,13 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "visualization_msgs/msg/marker.hpp"
+#include "std_msgs/msg/string.hpp"
+
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 
 namespace husky_highlevel_controller
 {
@@ -23,25 +30,82 @@ namespace husky_highlevel_controller
       declare_parameter("queue_size", 10, queue_param_desc);
 
       param_callback_handel_ = add_on_set_parameters_callback(
-        std::bind(&Subscriber::handleOnSetParameter, this, std::placeholders::_1)
-      );
-      
+          std::bind(&Subscriber::handleOnSetParameter, this, std::placeholders::_1));
+
       rclcpp::QoS qos(rclcpp::KeepLast(get_parameter("queue_size").as_int()));
       subscriber_ = create_subscription<sensor_msgs::msg::LaserScan>(
           get_parameter("topic").as_string(),
           qos,
           std::bind(&Subscriber::callback, this, std::placeholders::_1));
+
+      publisher_ = create_publisher<geometry_msgs::msg::Twist>("/a200_0000/cmd_vel", 10);
+      publisher_estimate_ = create_publisher<visualization_msgs::msg::Marker>("/estimate", 10);
+      tf_buffer_ = std::make_shared<tf2_ros::Buffer>(get_clock());
+      tf_listener_ = std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
     }
 
   private:
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscriber_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr publisher_estimate_;
     OnSetParametersCallbackHandle::SharedPtr param_callback_handel_;
+
+    tf2_ros::Buffer::SharedPtr tf_buffer_;
+    std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
 
   private:
     void callback(const sensor_msgs::msg::LaserScan &msg)
     {
       auto min = std::min_element(msg.ranges.begin(), msg.ranges.end());
-      RCLCPP_INFO_STREAM(get_logger(), "Minimum range: " << *min);
+      auto min_angle = msg.angle_min + std::distance(msg.ranges.begin(), min) * msg.angle_increment;
+      auto distance = *min;
+      // turn robot to angle and move in direction of the minimum range
+      geometry_msgs::msg::Twist twist;
+      // check if the minimum distance is less than 0.5m and not infinity
+      if (distance > 0.0 && distance != std::numeric_limits<float>::infinity())
+        twist.linear.x = distance;
+      twist.angular.z = min_angle;
+      publisher_->publish(twist);
+
+      geometry_msgs::msg::Point trans;
+
+      try
+      {
+        std::string source = "lidar2d_0_laser";
+        std::string target = "odom";
+        if (tf_buffer_->canTransform(target, source, tf2::TimePointZero))
+        {
+          auto transform = tf_buffer_->lookupTransform(target, source, tf2::TimePointZero);
+          geometry_msgs::msg::Point point;
+          point.x = distance * cos(min_angle);
+          point.y = distance * sin(min_angle);
+          tf2::doTransform(point, trans, transform);
+        }
+      }
+      catch (const tf2::TransformException &e)
+      {
+        RCLCPP_ERROR_STREAM(get_logger(), e.what());
+        return;
+      }
+
+      // for odom baselink
+      visualization_msgs::msg::Marker estimate_laser;
+      estimate_laser.header.frame_id = "odom";
+      estimate_laser.header.stamp = now();
+      estimate_laser.id = 0;
+      estimate_laser.type = visualization_msgs::msg::Marker::CYLINDER;
+      estimate_laser.action = visualization_msgs::msg::Marker::MODIFY;
+      estimate_laser.pose.position = trans;
+      estimate_laser.scale.x = 0.5;
+      estimate_laser.scale.y = 0.5;
+      estimate_laser.scale.z = 0.5;
+      estimate_laser.color.a = 1.0;
+      estimate_laser.color.r = 0.0;
+      estimate_laser.color.g = 1.0;
+      estimate_laser.color.b = 0.0;
+      publisher_estimate_->publish(estimate_laser);
+
+      RCLCPP_INFO_STREAM(get_logger(), "Minimum range: " << distance << " at angle: " << min_angle);
     }
 
     rcl_interfaces::msg::SetParametersResult handleOnSetParameter(
@@ -63,25 +127,25 @@ namespace husky_highlevel_controller
                             << param.value_to_string() << "\"");
           if (param.get_name() == "queue_size")
             queue_size = param.as_int();
-          
+
           if (param.get_name() == "topic")
             topic = param.as_string();
         }
       }
       RCLCPP_INFO_STREAM(get_logger(), "Creating new subscriber with topic: " << topic << " and queue size: " << queue_size);
-      
+
       auto new_subscriber = create_subscription<sensor_msgs::msg::LaserScan>(
           topic,
           rclcpp::QoS(rclcpp::KeepLast(queue_size)),
           std::bind(&Subscriber::callback, this, std::placeholders::_1));
       subscriber_.swap(new_subscriber);
-      
+
       rcl_interfaces::msg::SetParametersResult result;
       result.successful = true;
       return result;
     }
-  }; 
-} 
+  };
+}
 
 int main(int argc, char *argv[])
 {
